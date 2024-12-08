85d93665cdb70ef8c445dde8f0d8c49e16a12788
jerich931 <jerichlee2@gmail.com>
2024-12-07 20:23:23 -0600

    asdf

--- scripts/math-tex-to-html.py
+++ scripts/math-tex-to-html.py
@@ -10,7 +10,6 @@ def latex_to_html(latex_file, output_html, css_path):
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Converted Document</title>
     <link rel="stylesheet" href="{css_path}">
     <script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
     <script>

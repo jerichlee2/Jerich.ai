573da8c6b2190929ddd3481d5524550eb15d87f0
jerich931 <jerichlee2@gmail.com>
2025-06-10 02:34:22 -0500

    Add diary entry metadata: TBL-25 — Experimental Bone Mechanics

--- posts.js
+++ posts.js
@@ -4,15 +4,16 @@
  */
 const posts = [
    
-  { date:"2025-06-09", title:"Lebesgue-Integration", category:"Mathematics", class:"MATH-540", tags:["Lebesgue-Integration","Measure-Theory"] },
-  { date:"2025-06-10", title:"Fundamental Math", category:"Mathematics", class:"MATH-447", tags:["Real Analysis"] },
-  { date:"2025-06-10", title:"Limits and sequences", category:"Mathematics", class:"MATH-447", tags:["Real Analysis"] },
-  { date:"2025-06-10", title:"Limits and sequences continued", category:"Mathematics", class:"MATH-447", tags:["Real analysis"] },
-  { date:"2025-06-10", title:"Metric spaces and topology", category:"Mathematics", class:"MATH-447", tags:["Real Analysis","topology","set theory"] },
-  { date:"2025-06-10", title:"Convergent series", category:"Mathematics", class:"MATH-447", tags:["Real Analysis"] },
-  { date:"2025-06-10", title:"Continuity", category:"Mathematics", class:"MATH-447", tags:["Real Analysis","topology","set theory"] },
-  { date:"2025-06-10", title:"Connectedness and Uniform Continuity", category:"Mathematics", class:"MATH-447", tags:["Real Analysis","topology"] },
-  { date:"2025-06-10", title:"Uniform Convergence", category:"Mathematics", class:"MATH-447", tags:["real analysis"] },
-  { date:"2025-06-10", title:"Differentiation and Integration", category:"Mathematics", class:"MATH-447", tags:["real analysis","integration","differentiation"] },
-  { date:"2025-06-10", title:"The Fundamental Theorem of Calculus", category:"Mathematics", class:"MATH-447", tags:["real analysis","ftc"] },
+  { date:"2025-06-09", title:"Lebesgue-Integration", category:"Mathematics", class:"MATH-540", folder:"06-09", tags:["Lebesgue-Integration","Measure-Theory"] },
+  { date:"2025-06-10", title:"Fundamental Math", category:"Mathematics", class:"MATH-447", folder:"06-10", tags:["Real Analysis"] },
+  { date:"2025-06-10", title:"Limits and sequences", category:"Mathematics", class:"MATH-447", folder:"06-10.1", tags:["Real Analysis"] },
+  { date:"2025-06-10", title:"Limits and sequences continued", category:"Mathematics", class:"MATH-447", folder:"06-10.2", tags:["Real analysis"] },
+  { date:"2025-06-10", title:"Metric spaces and topology", category:"Mathematics", class:"MATH-447", folder:"06-10.3", tags:["Real Analysis","topology","set theory"] },
+  { date:"2025-06-10", title:"Convergent series", category:"Mathematics", class:"MATH-447", folder:"06-10.4", tags:["Real Analysis"] },
+  { date:"2025-06-10", title:"Continuity", category:"Mathematics", class:"MATH-447", folder:"06-10.5", tags:["Real Analysis","topology","set theory"] },
+  { date:"2025-06-10", title:"Connectedness and Uniform Continuity", category:"Mathematics", class:"MATH-447", folder:"06-10.6", tags:["Real Analysis","topology"] },
+  { date:"2025-06-10", title:"Uniform Convergence", category:"Mathematics", class:"MATH-447", folder:"06-10.7", tags:["real analysis"] },
+  { date:"2025-06-10", title:"Differentiation and Integration", category:"Mathematics", class:"MATH-447", folder:"06-10.8", tags:["real analysis","integration","differentiation"] },
+  { date:"2025-06-10", title:"The Fundamental Theorem of Calculus", category:"Mathematics", class:"MATH-447", folder:"06-10.9", tags:["real analysis","ftc"] },
+  { date:"2025-06-10", title:"Experimental Bone Mechanics", category:"Engineering", class:"TBL-25", folder:"06-10", tags:["Experiment","Bone Mechanics","Testing"] },
   ];
